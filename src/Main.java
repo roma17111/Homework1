895c0a5f0a1af4910c60263aebfe3e67a0e92c5c
Роман <romanze1706@gmail.com>
2022-11-02 14:01:49 +0300

    проверьте задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789.0;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
     }
 }
